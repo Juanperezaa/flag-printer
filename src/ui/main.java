9a13d809a67ae4cb5c74ddd87be2cf45a8384c14
Sebastián Pérez <sebas8041@hotmail.com>
2021-05-21 20:12:25 -0500

    nothing

--- src/ui/main.java
+++ src/ui/main.java
@@ -1,5 +1,21 @@
 package ui;
 
 public class main {
-
+	static String b = "\u001B[0m";       
+	static String negro = "\033[30m";
+	static String rojo = "\033[31m";
+	static String verde = "\033[32m";
+	static String amarillo = "\033[33m";
+	static String azul = "\033[34m";
+	static String magenta = "\033[35m";
+	static String celeste = "\033[36m";
+	static String blanco = "\033[37m";
+	        
+	static String fRojo = "\033[41m";
+	static String fVerde = "\033[42m";
+	static String fAmarillo = "\033[43m";
+	static String fAzul = "\033[44m";
+	static String fMagenta = "\033[45m";
+	static String fCeleste = "\033[46m";
+	static String fGris = "\033[47m";
 }
